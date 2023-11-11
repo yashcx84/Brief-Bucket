64006e8f98e446f20ed5b3ebb513805bad30d95c
Yash Jangid <yashcx84@gmail.com>
2023-11-11 17:20:29 +0530

    initial commit.

---
+++ index.js
@@ -0,0 +1,6 @@
+// let number = prompt("Describe Yourself in just 20 Characters.");
+// let Nnumber = number.length;
+// let CharactersLeft = (20 - Nnumber);
+
+// let rndtext = document.getElementById("char");
+// rndtext.innerHTML = CharactersLeft;
